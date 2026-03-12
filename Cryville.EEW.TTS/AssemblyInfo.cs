a2fbcf22c28f18d776d2da1873b0207e2126c8f0
PopSlime <popslime@qq.com>
2026-03-12 15:11:45 +0800

    chore: Apply UsesExtendedSemanticVersioning to interfaces

--- Cryville.EEW.TTS/AssemblyInfo.cs
+++ Cryville.EEW.TTS/AssemblyInfo.cs
@@ -1,4 +1,6 @@
+using Cryville.EEW.ComponentModel;
 using System.Runtime.CompilerServices;
 
+[assembly: UsesExtendedSemanticVersioning]
 [assembly: InternalsVisibleTo("Cryville.EEW.Core")]
 [assembly: InternalsVisibleTo("Cryville.EEW.Test")]
