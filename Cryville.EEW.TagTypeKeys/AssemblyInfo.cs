c96aac919e0d97eea31bf219d432960b0bce4d88
PopSlime <popslime@qq.com>
2026-03-13 15:36:13 +0800

    chore: Apply metadata attributes to new interfaces

---
+++ Cryville.EEW.TagTypeKeys/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Cryville.EEW.ComponentModel;
+using System.Runtime.CompilerServices;
+
+[assembly: UsesExtendedSemanticVersioning]
+[assembly: InternalsVisibleTo("Cryville.EEW.Core")]
+[assembly: InternalsVisibleTo("Cryville.EEW.Test")]
